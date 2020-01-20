640153786c8f1f218eea2cb699d0031aed4c2a2b
Gary Scott <gary@garyscott.net>
2020-01-19 17:49:42 -0800

    makeAlbumIds OBJ, wip

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const slugify = require('slugify');
 
-const makeAlbumId = path => taolagSlugWithoutLastSegment(path);
-
-const makeParentAlbumId = path => taolagSlugWithoutTwoLastSegment(path);
-
 const makePhotoId = path => taolagSlug(path);
 
 /**
@@ -20,73 +16,20 @@ const taolagSlug = input =>
  * remove last '/' and anything after
  * 'Hello/World' => 'hello'
  */
-const getAlbumId = path => path.substring(0, path.lastIndexOf('/'));
+const makeAlbumIds = path => {
+  const albumId = taolagSlug(path.substring(0, path.lastIndexOf('/')));
 
-const getAlbumParentId = path => {
-  const first = getAlbumId(path);
+  const parentAlbumId = albumId.substring(0, albumId.lastIndexOf('/'));
+  console.log(parentAlbumId);
 
-  const second = first.substring(0, first.lastIndexOf('/'));
-
-  return second ? second : '_';
-};
-
-/**
- * remove last '/' and anything after
- * 'Hello/World' => 'hello'
- */
-const taolagSlugWithoutLastSegment = input =>
-  removeSegments(taolagSlug(input), 1, '_');
-
-/**
- * remove last 2 '/' and anything after
- * 'Leroy Jackson/Hello/World => 'leroy-jackson'
- */
-const taolagSlugWithoutTwoLastSegment = input =>
-  removeSegments(taolagSlug(input), 2, '_');
-
-/**
- *
- * @param {*} input
- * @param {*} num
- * @param {*} ifEmpty
- *
- * If result is blank will return the ifEmpty
- */
-const removeSegments = (input, num = 1, ifEmpty = '') => {
-  const index = num - 1;
-  //const lastIndex = input.lastIndexOf('/') - index;
-
-  //const result = input.substring(0, input.nthLastIndexOf('/',num));
-
-  secondLastIndex = input.lastIndexOf('/', input.lastIndexOf('/') - 1);
-
-  // console.log(result);
-  return result ? result : ifEmpty;
-};
-
-/**
- * https://stackoverflow.com/a/55679985/2057438
- * url.nthLastIndexOf('/', 2);
- * url.nthLastIndexOf('/', 3);
- * url.nthLastIndexOf('/');
- */
-String.prototype.nthLastIndexOf = function(searchString, n) {
-  var url = this;
-  if (url === null) {
-    return -1;
-  }
-  if (!n || isNaN(n) || n <= 1) {
-    return url.lastIndexOf(searchString);
-  }
-  n--;
-  return url.lastIndexOf(searchString, url.nthLastIndexOf(searchString, n) - 1);
+  return {
+    albumId: albumId,
+    parentAlbumId: parentAlbumId ? parentAlbumId : '_'
+  };
 };
 
 module.exports = {
-  makeAlbumId,
-  makeParentAlbumId,
+  taolagSlug,
   makePhotoId,
-  getAlbumId,
-  getAlbumParentId
-  
+  makeAlbumIds
 };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const sut = require('../index');
 
+// Slug
+
 function taolagSlug_IsValid() {
   const input = 'The Super Rad/Thing/With - Dash - AND.DOT';
   const expected = 'the-super-rad/thing/with-dash-and.dot';
@@ -9,112 +11,40 @@ function taolagSlug_IsValid() {
   console.log(`taolagSlug_IsValid: ${actual === expected}: ${actual}`);
 }
 
+// Album ID
 
-
-
-function taolagSlugWithoutLastSegment_IsValid() {
-  const input = 'The Super Rad/Thing/With - Dash - AND.DOT';
-  const expected = 'the-super-rad/thing';
-
-  const actual = sut.taolagSlugWithoutLastSegment(input);
-
-  console.log(
-    `taolagSlugWithoutLastSegment_IsValid: ${actual ===
-      expected}: ${actual}`
-  );
-}
-
-function taolagSlugWithoutTwoLastSegment_IsValid() {
-  const input = 'The Super Rad/Thing/With - Dash - AND.DOT';
-  const expected = 'the-super-rad';
-
-  const actual = sut.taolagSlugWithoutLastSegment(input);
-
-  console.log(
-    `taolagSlugWithoutTwoLastSegment_IsValid: ${actual ===
-      expected}: ${actual}`
-  );
-}
-
-function taolagSlugWithoutTwoLastSegment_ReturnsUnderscore() {
-  const input = 'The Super Rad/Thing.yaml';
-  const expected = '_';
-
-  const actual = sut.taolagSlugWithoutLastSegment(input);
-
-  console.log(
-    `taolagSlugWithoutTwoLastSegment_ReturnsUnderscore: ${actual ===
-      expected}: ${actual}`
-  );
-}
-
-
-
-function makeTokenUrl_IsValid() {
-  const input = 'The Super Rad/Thing/With - Dash - AND.DOT';
-  const expected = 'the-super-rad:thing:with-dash-and.dot';
-
-  const actual = sut.makeTokenUrl(input);
-
-  console.log(`makeTokenUrl_IsValid: ${actual === expected}: ${actual}`);
-}
-
-
-
-function alId1(){
-  const input = 'The Super Rad/Thing/With - Dash - AND.DOT';
-  const expected = 'The Super Rad/Thing';
-
-  const actual = sut.getAlbumId(input);
-
-  console.log(`alId1: ${actual === expected}: ${actual}`);
-
-}
-
-function alId2(){
-  const input = 'The Super Rad/Thing/Again/With - Dash - AND.DOT';
-  const expected = 'The Super Rad/Thing/Again';
-
-  const actual = sut.getAlbumId(input);
-
-  console.log(`alId2: ${actual === expected}: ${actual}`);
-
-}
-
-function alParId1(){
-  const input = 'The Super Rad/Thing/Again/With - Dash - AND.DOT';
-  const expected = 'The Super Rad/Thing';
-
-  const actual = sut.getAlbumParentId(input);
-
-  console.log(`alPId1: ${actual === expected}: ${actual}`);
-
-}
-function alParId2(){
+function makeAlbumIds_IsValid1() {
   const input = 'The Super Rad/With - Dash - AND.DOT';
-  const expected = '_';
+  const expected = { albumId: 'the-super-rad', parentAlbumId: '_' };
 
-  const actual = sut.getAlbumParentId(input);
+  const actual = sut.makeAlbumIds(input);
 
-  console.log(`alPId2: ${actual === expected}: ${actual}`);
-
+  console.log(
+    `alObj: ${JSON.stringify(actual) ===
+      JSON.stringify(expected)} : ${JSON.stringify(actual)}`
+  );
 }
 
+function makeAlbumIds_IsValid2() {
+  const input = 'The Super Rad/Thing - 1/With - Dash - AND.DOT';
+  const expected = {
+    albumId: 'the-super-rad/thing-1',
+    parentAlbumId: 'the-super-rad'
+  };
 
+  const actual = sut.makeAlbumIds(input);
 
+  console.log(
+    `alObj: ${JSON.stringify(actual) ===
+      JSON.stringify(expected)} : ${JSON.stringify(actual)}`
+  );
+}
 
-alId1();
-alId2();
-alParId1();
-alParId2();
+// Photo ID
 
+// Convert URL
 
-//taolagSlug_IsValid();
-
-//taolagSlugWithoutLastSegment_IsValid();
-//taolagSlugWithoutTwoLastSegment_IsValid();
-//taolagSlugWithoutTwoLastSegment_ReturnsUnderscore();
-
-//makeTokenUrl_IsValid();
-
-
+// Runners
+taolagSlug_IsValid();
+makeAlbumIds_IsValid1();
+makeAlbumIds_IsValid2();
